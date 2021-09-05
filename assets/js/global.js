dafdab379b8ed79019325199f9d9286ab83e3623
abmateen <abmateen1994@gmail.com>
2021-09-06 03:07:25 +0500

    code update

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,4 @@
+// Multi step Forms jQuery
 $(document).ready(function() {
     $('#givTip .close').click(function() {
         $('.tip-payment').show('3000');
@@ -9,3 +10,25 @@ $(document).ready(function() {
         $('.multi-step-form .success-msg').show('3000');
     });
 });
+// Stop AutoSlide post slider 
+$('.carousel').carousel({
+    interval: false,
+});
+// Hide Post Slider controls on first item
+$(document).ready(function() {
+    checkslideitem();
+});
+$('#postPopupSlider').on('slid.bs.carousel', checkslideitem);
+
+function checkslideitem() {
+    var $this = $('#postPopupSlider');
+    if ($('.carousel-inner .carousel-item:first').hasClass('active')) {
+        // Hide left arrow
+        $this.children('.carousel-control-prev').hide();
+    } else if ($('.carousel-inner .carousel-item:last').hasClass('active')) {
+        // Hide left arrow
+        $this.children('.carousel-control-next').hide();
+    } else {
+        $this.children('.carousel-control-next, .carousel-control-prev').show();
+    }
+}
