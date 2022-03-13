bba7b6fa1c9aaffaf2796a2b22996a2c51bf2771
Ab Mateen <abmateen1994@gmail.com>
2022-03-14 04:19:53 +0500

    add new modules

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,38 +1,30 @@
 // Multi step Forms jQuery
 $(document).ready(function() {
+    // Subscription Enable or disbale
+    if ($('#subsPermit').prop('checked') === false) {
+        $('.monthly_subscription').addClass('disabled');
+    };
+    $('#subsPermit').change(function() {
+        if (this.checked) {
+            $('.monthly_subscription').removeClass('disabled');
+        } else {
+            $('.monthly_subscription').addClass('disabled');
+        }
+    });
+    $('.soporte_tab .nav-link').click(function() {
+        $('.ticket_tabs .tab-pane').removeClass('active');
+        // $('#tabActivos').addClass('active');
+    });
+    // Tickets
+    $('.ticket_content .nav-link').click(function() {
+        var itTarget = $(this).attr('href');
+        $('#tabActivos').removeClass('active');
+        $(itTarget).addClass('active show');
+    });
+
     $('#givTip .close').click(function() {
         $('.tip-payment').show('3000');
         $('.tip-success').hide('3000');
-    });
-    // MUlti Step Forms Like click and come a Thanks msg
-    // Controling with simple data target
-    $('.multi-step-form .success-msg').hide('3000');
-    $('.js-form-next').on('click', function() {
-        var targetId = $(this).data('target')
-        $('.multi-step-form .js-form-container').hide('3000');
-        $('#' + targetId).show('3000');
-    });
-
-    // For Adding Payment Method on Profile
-    $('[class*="bank-account-"]').hide();
-    $('.js-bank-sub').on('click', function() {
-        var targetId = $(this).data('target');
-        $(targetId).show('3000', function() {
-            $(targetId).addClass('is-show');
-            if ($(targetId).hasClass('is-show')) {
-                $('#tabBancarios .alert').hide('1000')
-            }
-            if ($('#detailsMercado').hasClass('is-show')) {
-                $('#mercadoFields').addClass('is-disabled');
-            } else {
-                $('#mercadoFields').removeClass('is-disabled');
-            }
-            if ($('#detailsBanco').hasClass('is-show')) {
-                $('#bancoFields').addClass('is-disabled');
-            } else {
-                $('#bancoFields').removeClass('is-disabled');
-            }
-        });
     });
     // Hide On click Close
     $('.js-close-btn').on('click', function() {
@@ -65,53 +57,13 @@ function checkslideitem() {
         $this.children('.carousel-control-next, .carousel-control-prev').show();
     }
 }
-// For Input Country Select
-//test for getting url value from attr
-// var img1 = $('.test').attr("data-thumbnail");
-// console.log(img1);
 
-//test for iterating over child elements
-var langArray = [];
-$('.vodiapicker option').each(function() {
-    var img = $(this).attr("data-thumbnail");
-    var text = this.innerText;
-    var value = $(this).val();
-    var item = '<li><img src="' + img + '" alt="" value="' + value + '"/><span>' + text + '</span></li>';
-    langArray.push(item);
-})
 
-$('#a').html(langArray);
-
-//Set the button value to the first el of the array
-$('.btn-select').html(langArray[0]);
-$('.btn-select').attr('value', 'en');
-
-//change button stuff on click
-$('#a li').click(function() {
-    var img = $(this).find('img').attr("src");
-    var value = $(this).find('img').attr('value');
-    var text = this.innerText;
-    var item = '<li><img src="' + img + '" alt="" /><span>' + text + '</span></li>';
-    $('.btn-select').html(item);
-    $('.btn-select').attr('value', value);
-    $(".b").toggle();
-    //console.log(value);
+// Telephone INput Select
+// you Can Find the Options Here
+// https://github.com/jackocnr/intl-tel-input
+$("#telNumber").intlTelInput({
+    autoHideDialCode: false,
+    separateDialCode: true,
+    initialCountry: "ar",
 });
-
-$(".btn-select").click(function() {
-    $(".b").toggle();
-});
-
-//check local storage for the lang
-var sessionLang = localStorage.getItem('lang');
-if (sessionLang) {
-    //find an item with value of sessionLang
-    var langIndex = langArray.indexOf(sessionLang);
-    $('.btn-select').html(langArray[langIndex]);
-    $('.btn-select').attr('value', sessionLang);
-} else {
-    var langIndex = langArray.indexOf('ch');
-    // console.log(langIndex);
-    $('.btn-select').html(langArray[langIndex]);
-    //$('.btn-select').attr('value', 'en');
-}
